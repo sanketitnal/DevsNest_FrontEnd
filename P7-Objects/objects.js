f2fabf9bb60bc79116a2998cae03de53d7da80be
sanketitnal <sanketitnal20@gmail.com>
2021-06-19 22:15:05 +0530

    small modification in P6

--- P7-Objects/objects.js
+++ P7-Objects/objects.js
@@ -43,7 +43,7 @@ function Cylinder(radius, height) {
     this.height = height;
 }
 Cylinder.prototype.getVolume = function() {
-    return 2 * this.radius**2 * this.height * Math.PI;
+    return Math.PI * this.radius**2 * this.height;
 }
 
 /*
